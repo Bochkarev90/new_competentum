8bdacd84c17e2939f86e1c7f00a042b4823249ca
Bochkarev90 <bochkarev.rabota@gmail.com>
2019-04-11 16:26:48 +0300

    Pass

--- input/variables.py
+++ input/variables.py
@@ -5,7 +5,7 @@ revel = 1 if platform == 'r' else 0
 browser = 'f'  # 'c' - chrome, 'f' - firefox
 path = 'C:/work/Manza.xlsx' if revel else 'C:/work/Price.xlsx'  # Путь до excel-файла
 username = 'manza2.5e.cqa.student10@mailinator.com' if revel else 'e2review'
-password = 'Password1' if revel else 'Test1234'
+password = ''
 book = 'Manza_031119_TSP' if revel else 'Globalization and Diversity: Geography of a Changing World, 6e'  # Название курса
 chapter_number = 10  # Номер проверяемой главы
 set_number = 1  # Номер сета
